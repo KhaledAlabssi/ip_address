600372538fa6dd926acb94a334da935ece8c3244
KhaledAlabssi <khaled.absi5@gmail.com>
2021-07-10 10:07:04 +0200

    adding two boxes for map and Ip sections

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { useState, useEffect } from 'react';
-import './App.css';
+
 import 'rsuite/dist/styles/rsuite-default.css';
+import { Container, Header, Content, Footer, Navbar, FlexboxGrid, Panel, Col} from 'rsuite';
+import './App.css';
 
 function App() {
   const [ip, setIp] = useState('')
@@ -12,10 +14,38 @@ function App() {
   })
   
   return (
-    <div className="App">
-      <h1>{ip}</h1>
+    // <div className="App">
+    //   <h1>{ip}</h1>
       
-    </div>
+    // </div>
+    <div className="show-fake-browser login-page">
+    <Container style={{'height': '100vh'}}>
+      <Header>
+        <Navbar appearance="inverse">
+          <Navbar.Header>
+            <a className="navbar-brand logo">Brand</a>
+          </Navbar.Header>
+        </Navbar>
+      </Header>
+      <Content style={{padding: 25}}>
+        <FlexboxGrid justify="space-between">
+          <FlexboxGrid.Item componentClass={Col} colspan={24} md={12}>
+            <Panel header={<h3>IP Details</h3>} bordered >
+              <h4>Your IP is: {ip}</h4>
+              
+            </Panel>
+          </FlexboxGrid.Item>
+          <FlexboxGrid.Item componentClass={Col} colspan={24} md={12}>
+            <Panel header={<h3>Map</h3>} bordered >
+              
+            </Panel>
+          </FlexboxGrid.Item>
+        </FlexboxGrid>
+        
+      </Content>
+      <Footer>Footer</Footer>
+    </Container>
+  </div>
   );
 }
 
