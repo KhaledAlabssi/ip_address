8cb9f4e2de97eb61b7bcf82ac8e6ec50181b6d8d
mihail <mihail.soltan@protonmail.ch>
2021-07-14 22:34:55 +0200

    added env. file, added map and data from the rest countries api

--- src/component/Ip.js
+++ src/component/Ip.js
@@ -1,17 +1,41 @@
-export default function Ip({ ip, location }) {
+export default function Ip({ ip, location, capital, language, population, currency, flag, userData }) {
     // ip is the user ip
     // location is iso-2 output for Germany will be: "DE"
-
+    // console.log(location)
+   if(userData){
     return (
+
+
         <>
-            <h4>Your Ip is: {ip}</h4>
-            
-            <p>Your Country: {location.country}</p>
-            <p>Your Region: {location.region}</p>
-            <p>Your City: {location.city}</p>
-            <p>Lat: {location.lat}</p>
-            <p>Lng: {location.lng}</p>
-            <p></p>
+        
+                    <h4>Your Ip is: {ip}</h4>
+                     <img src={flag} style={{width: 100, height: 75}}></img>
+                    <p>Your Country: {location.country}</p>
+                    <p>Your Region: {location.region}</p>
+                    <p>Your City: {location.city}</p>
+                    <p>Lat: {location.lat}</p>
+                    <p>Lng: {location.lng}</p>
+                    <p>Capital: {capital}</p>
+                    <p>Language: {language}</p>
+                    <p>Population: {population}</p>
+                    <p>Currency: {currency}</p>
+                    
+                    
         </>
-    )
+            
+            
+            /* <p>Capital: {capital} </p> */ 
+    /* <p>Language: {language} </p> */ 
+    /* <p>Population: {population}</p> */ 
+    /* <p>Currency: {currency}</p> */ 
+    /* <p>Flag: <img src={flag} style={{width: 100, height: 50}} alt = "flag"></img></p> */ 
+       
+        
+    )} else{
+        return (
+            <>
+            <h1>Loading</h1>
+            </>
+        )
+    }
 }
