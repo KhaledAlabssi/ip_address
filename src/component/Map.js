d3ba2304bfd74982dcaf31f70656b502ebfbcdc8
mihail <mihail.soltan@protonmail.ch>
2021-07-20 22:10:34 +0200

    fixed marker

--- src/component/Map.js
+++ src/component/Map.js
@@ -1,5 +1,15 @@
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+
+let DefaultIcon = L.icon({
+    iconUrl: icon,
+    shadowUrl: iconShadow
+});
+
+L.Marker.prototype.options.icon = DefaultIcon;
 export default function Map({location}) {
     // console.log(location)
     if(location){
@@ -16,7 +26,7 @@ export default function Map({location}) {
             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
-          <Marker position={[51.53443,9.93228]}>
+          <Marker position={[location.lat, location.lng]}>
             <Popup>
               A pretty CSS3 popup. <br /> Easily customizable.
             </Popup>
